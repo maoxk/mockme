f8e5053ba487d6b487e6154462ca06a125ea733b
毛锡凯 <xikai.maoxk@alibaba-inc.com>
2011-11-15 23:20:28 +0800

    first commit

---
+++ mockme.viewer/mockme.viewer.backend/src/main/java/com/alibaba/cbu/mockme/viewer/App.java
@@ -0,0 +1,13 @@
+package com.alibaba.cbu.mockme.viewer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
